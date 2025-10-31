d63f8f3d6e87f88a371014af23369e4142588ef7
Votre Sylla <fatoumatbinetousylla@gmail.com>
2025-10-31 17:06:56 +0000

    Increase API rate limit to 1000 requests per minute

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -25,7 +25,7 @@ class RouteServiceProvider extends ServiceProvider
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(100)->by($request->ip());
+            return Limit::perMinute(1000)->by($request->ip());
         });
 
         RateLimiter::for('user-daily', function (Request $request) {
