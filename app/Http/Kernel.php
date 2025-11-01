bbb6998c16bc9bd5fe5f8d4cd28e0f417a00a817
Votre Sylla <fatoumatbinetousylla@gmail.com>
2025-11-01 22:40:25 +0000

    Add AuthMiddleware and RoleMiddleware for API authentication and authorization

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,6 +55,8 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
     'auth' => \App\Http\Middleware\Authenticate::class,
+    'auth.api' => \App\Http\Middleware\AuthMiddleware::class,
+    'role' => \App\Http\Middleware\RoleMiddleware::class,
     'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
     'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
     'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
