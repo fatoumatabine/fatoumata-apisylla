d371f581d46b01e1fc50d3af298136c89b44a518
Votre Sylla <fatoumatbinetousylla@gmail.com>
2025-11-02 21:44:38 +0000

    fix: Changer le guard API pour utiliser Passport au lieu de Sanctum

--- config/auth.php
+++ config/auth.php
@@ -44,8 +44,8 @@ return [
             'driver' => 'sanctum',
             'provider' => 'users',
         ],
-        'api' => [ // Utiliser Sanctum pour l'API
-            'driver' => 'sanctum',
+        'api' => [ // Utiliser Passport pour l'API
+            'driver' => 'passport',
             'provider' => 'users',
         ],
     ],
